bb26b6ca43128e655100a5478046e41b4d74fa4c
joanasdasd <33759606+Joanmascastella@users.noreply.github.com>
2023-09-24 11:40:56 +0200

    connected database to program

---
+++ DAL/MongoDBConnection.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Driver;
+
+namespace DAL;
+
+public class MongoDBConnection
+{
+    protected static IMongoDatabase database;
+
+    public MongoDBConnection()
+    {
+        MongoClient client =
+            new MongoClient(
+                "mongodb+srv://Ted:*049810Tc*@gardengroupproject.rsow8xd.mongodb.net/?retryWrites=true&w=majority");
+        database = client.GetDatabase("GardenGroupProject");
+    }
+}
