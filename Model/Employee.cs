cc7e7b7fb607b7f6d097119de809e9faf16f9a87
Ador1717 <114223629+Ador1717@users.noreply.github.com>
2023-09-28 14:41:20 +0200

    In model layer .. i added user class and employee class

---
+++ Model/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    internal class Employee
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; } // Should store hashed and salted password, not plaintext.
+        public List<string> TicketIDs { get; set; } // List of tickets associated with the user.
+        public UserType Type { get; set; }
+    }
+
+    public enum UserType
+    {
+        Regular,
+        ServiceDesk
+    }
+}
